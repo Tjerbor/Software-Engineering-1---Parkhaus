f2ec3935614a1531b512bc4ae5a30416adf4ec71
Samar reguigui <reguiguisamar4@gmail.com>
2023-04-18 17:17:25 +0200

    add ticket im TicketDatenbank(T08)

--- src/main/java/TicketDatenbank.java
+++ src/main/java/TicketDatenbank.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketDatenbank {
+
+    private List<Ticket> ticketdatenbank ;
+
+    public TicketDatenbank(List<Ticket> ticketdatenbank) {
+        this.ticketdatenbank = new ArrayList<Ticket>();;
+    }
+
+    public void addticket (Ticket ticket){
+        ticketdatenbank.add(ticket);
+    }
 }
