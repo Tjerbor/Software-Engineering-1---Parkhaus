dacb01a871dcd988166fcb7b7d7e0e60c710363e
Samar reguigui <reguiguisamar4@gmail.com>
2023-04-23 18:53:06 +0200

    new Parkhaus

--- src/main/java/Servlets/HelloServlet.java
+++ src/main/java/Servlets/HelloServlet.java
@@ -1,5 +1,7 @@
 package Servlets;
 
+import Classes.Parkhaus;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,12 +9,14 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+   public static Parkhaus parkhaus = new Parkhaus();
 
     public void init() {
         message = "Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         response.setContentType("text/html");
 
         // Hello
