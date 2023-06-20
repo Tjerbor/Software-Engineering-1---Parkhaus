43038959d60cd4640206e8d065ea970dd3e04cc8
Thiemo Ottersbach <thiemo.ott@gmail.com>
2023-06-20 18:06:13 +0200

    [Thiemo] Simple Clean-Up

--- src/main/java/Servlets/KassenautomatServlet.java
+++ src/main/java/Servlets/KassenautomatServlet.java
@@ -43,9 +43,7 @@ public class KassenautomatServlet extends HttpServlet {
             servCon.setAttribute("ticketID", id);
         }
 
-
         TicketDatenbank datenbank = Parkhaus.getTicketDatenbank();
-
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -67,57 +65,7 @@ public class KassenautomatServlet extends HttpServlet {
             out.println("<h1>Ticket jibbit nicht.</h1>");
         }
 
-
-        /*if (ticketID != null && datenbank.containsTicket(ticketID)) {
-            Ticket ticket = datenbank.getTicket(ticketID);
-            String Zustand = ticket.getZustand();
-
-            out.println("<h1>Ticket: " + ticketID + "</h1>");
-            out.println("<h2>Zustand: " + Zustand + "</h2>");
-            String erstellDatum = ticket.getErstellDatum().toString();
-            out.println("<p>Erstellt am " + erstellDatum.substring(0, 10) + " um " + erstellDatum.substring(11, 16) + " Uhr.</p>");
-            out.println(String.format("<p>Parkdauer: %.2fh</p>", ticket.berechneParkdauer()));
-
-
-            if (Zustand.equals("bezahlt")) {
-                String bezahlDatum = ticket.getBezahlDatum().toString();
-                out.println("<p>Bezahlt am " + bezahlDatum.substring(0, 10) + " um " + bezahlDatum.substring(11, 16) + " Uhr.</p>");
-                servCon.setAttribute("tID", null);
-                out.println("<h4>Das Ticket wurde bezahlt. Bitte verlassen Sie das Parkhaus.</h4>");
-
-            } else if (Zustand.equals("erstellt") || Zustand.equals("Nachzahlung")) {
-                if (geld != null) {
-                    Double Geld = Double.parseDouble(geld);
-                    if (Geld != 0.0) {
-                        Kassenautomat.bezahle(ticket, Geld);
-                    }
-                }
-
-                double Bezahlt = ticket.getUeberwiesen();
-                double preis = Preis.getPreis(ticket) - ticket.getUeberwiesen();
-                out.println(String.format("<p>Zu zahlender Betrag: %.2f Euro</p>", round2Decimals(preis)));
-
-                if (Bezahlt != 0.0) {
-                    if (Bezahlt < preis) {
-                        out.println(String.format("<h4>Sie haben bereits %.2f Euro bezahlt. Es fehlen noch %.2f Euro, um das Ticket zu bezahlen.</h4>", round2Decimals(Bezahlt), round2Decimals(preis - Bezahlt)));
-                    } else {
-                        out.println(String.format("<h4>Sie haben zu viel bezahlt. Bitte buchen Sie -%.2f Euro wieder ab.</h4>", round2Decimals(Bezahlt - preis)));
-                    }
-                }
-
-                out.println("<form action=\"kassenautomat\" method=\"post\">\n" +
-                        "    <label for=\"geld\">Geld:</label>\n" +
-                        "    <input type=\"text\" id=\"geld\" name=\"geld\"><br><br>\n" +
-                        "    <input type=\"submit\" value=\"Submit\">\n" +
-                        "</form>");
-            }
-
-        } else {
-            out.println("<h1>Ticket jibbit nicht.</h1>");
-        }*/
-
         out.println("</body></html>");
-
     }
 
     public void destroy() {
