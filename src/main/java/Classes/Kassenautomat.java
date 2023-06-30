709fcd20a3325442bb87ef6718ea1a7624d1acb6
Cynthia Teguia <cynthia.teguia@smail.inf.h-brs.de>
2023-06-30 17:03:33 +0200

    methode bezahle auskommentiert,weil im code nicht verwendet ist

--- src/main/java/Classes/Kassenautomat.java
+++ src/main/java/Classes/Kassenautomat.java
@@ -1,16 +1,14 @@
 package Classes;
 
-import Classes.Tickets.Ticket;
-
 public class Kassenautomat {
 
 
-    public static void bezahle(Ticket ticket, double geld) {
+   // public static void bezahle(Ticket ticket, double geld) {
         //double preis = Preis.getPreis(ticket);
-        double diff = geld + ticket.getUeberwiesen();
+       // double diff = geld + ticket.getUeberwiesen();
 
-        ticket.setUeberwiesen(diff);
-    }
+       // ticket.setUeberwiesen(diff);
+    //}
 
     public static double round2Decimals(double input) {
         return ((double) ((int) ((input * 100) + 0.5))) / 100;
