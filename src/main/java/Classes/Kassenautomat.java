24ec45b2f2fbb35c0eaf4fd8d03261fe9e61ee3f
Bertrand Alougou <bertrand.alougou@yahoo.com>
2023-06-30 17:32:39 +0200

    Kassenautomat (Code fix) ---> privater Konstruktor [Bertrand]

--- src/main/java/Classes/Kassenautomat.java
+++ src/main/java/Classes/Kassenautomat.java
@@ -5,11 +5,11 @@ import Classes.Tickets.Ticket;
 public class Kassenautomat {
 
 private Kassenautomat(){}
-    public static void bezahle(Ticket ticket, double geld) {
-        double diff = geld + ticket.getUeberwiesen();
+    //public static void bezahle(Ticket ticket, double geld) {
+     //   double diff = geld + ticket.getUeberwiesen();
 
-        ticket.setUeberwiesen(diff);
-    }
+     //   ticket.setUeberwiesen(diff);
+   // }
 
     public static double round2Decimals(double input) {
         return ((double) ((int) ((input * 100) + 0.5))) / 100;
