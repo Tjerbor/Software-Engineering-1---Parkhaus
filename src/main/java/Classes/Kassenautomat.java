12d5f0bf58fb2b05a15264387a1745fda9e4ad54
Bertrand Alougou <bertrand.alougou@yahoo.com>
2023-06-30 17:35:17 +0200

    Kassenautomat (Code fix) ---> Kommentierte Zeilen gelöscht [Bertrand]

--- src/main/java/Classes/Kassenautomat.java
+++ src/main/java/Classes/Kassenautomat.java
@@ -1,15 +1,9 @@
 package Classes;
 
-//import Classes.Tickets.Ticket;
 
 public class Kassenautomat {
 
 private Kassenautomat(){}
-    //public static void bezahle(Ticket ticket, double geld) {
-     //   double diff = geld + ticket.getUeberwiesen();
-
-     //   ticket.setUeberwiesen(diff);
-   // }
 
     public static double round2Decimals(double input) {
         return ((double) ((int) ((input * 100) + 0.5))) / 100;
