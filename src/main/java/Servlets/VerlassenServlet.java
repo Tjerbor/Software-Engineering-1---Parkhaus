9f108dea76c63d4872af3de04e10adc93da9f22d
Bertrand Alougou <bertrand.alougou@yahoo.com>
2023-07-14 00:15:50 +0200

    VerlassenServlet css [Bertrand]

--- src/main/java/Servlets/VerlassenServlet.java
+++ src/main/java/Servlets/VerlassenServlet.java
@@ -11,22 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "verlassenServlet", value = "/verlassen-servlet")
+    @WebServlet(name = "verlassenServlet", value = "/verlassen-servlet")
+    public class VerlassenServlet extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
 
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title style=\"color: purple;\">Verlassen Servlet</title>");
+            out.println("<style>");
+            out.println("body { background-color: plum; text-align: center; font-family: Arial, sans-serif; }");
+            out.println("h1 { color: purple; font-size: 24px; }");
+            out.println("form { display: flex; flex-direction: column; align-items: center; margin-top: 20px; }");
+            out.println("input[type='text'] { margin-bottom: 10px; font-size: 16px; }");
+            out.println("input[type='submit'] { font-size: 16px; }");
+            out.println("</style>");
+            out.println("</head>");
+            out.println("<body>");
 
-public class VerlassenServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hallo lieber Kunde, bitte geben Sie ihr Ticket an.</h1>");
-        out.println("<form action=\"verlassen-servlet\" method=\"post\">\n" +
-                "    <label for=\"TicketID\">TicketID: \n" +
-                "    <input type=\"text\" id=\"TicketID\" name=\"TicketID\"><br><br>\n" +
-                "    <input type=\"submit\" value=\"Submit\">\n" +
-                "</form>");
+            out.println("<h1>Hallo lieber Kunde, bitte geben Sie Ihr Ticket ab.</h1>");
+            out.println("<form action=\"verlassen-servlet\" method=\"post\">\n" +
+                    "    <label for=\"TicketID\">TicketID: \n" +
+                    "    <input type=\"text\" id=\"TicketID\" name=\"TicketID\"><br><br>\n" +
+                    "    <input type=\"submit\" value=\"Submit\">\n" +
+                    "</form>");
 
-        out.println("</body></html>");
-    }
+            out.println("</body>");
+            out.println("</html>");
+        }
+
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String Id = request.getParameter("TicketID");
